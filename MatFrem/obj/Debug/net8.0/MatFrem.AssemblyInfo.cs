1b7938388ca2e094b8aa8de23bd5c7ae9977076f
DAkintola94 <degameros@gmail.com>
2024-11-14 23:41:41 +0100

    Api to display address name added

--- MatFrem/obj/Debug/net8.0/MatFrem.AssemblyInfo.cs
+++ MatFrem/obj/Debug/net8.0/MatFrem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatFrem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07200fe620010f9e9799935d0549253e17d13f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c484ad6c5a2f67c5b4877381194ef5a87f5e0504")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatFrem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatFrem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
