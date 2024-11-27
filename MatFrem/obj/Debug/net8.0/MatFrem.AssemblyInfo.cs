26ac8e6cc3328ff52d3f3ce2613b7adaded46e1a
DAkintola94 <degameros@gmail.com>
2024-11-27 02:17:49 +0100

    Frontpage etc, changes

--- MatFrem/obj/Debug/net8.0/MatFrem.AssemblyInfo.cs
+++ MatFrem/obj/Debug/net8.0/MatFrem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatFrem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed3dab91be6950559787a79794acdb5ec291057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df20264407216122606f05f88fbb48600c76e489")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatFrem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatFrem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
